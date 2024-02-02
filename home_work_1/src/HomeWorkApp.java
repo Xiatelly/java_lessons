9eb911500784f3a9d939b901b1fdef6e160e99fe
Dmitriy Nosov <xiatelly@gmail.com>
2024-02-02 22:32:23 +0200

    home_work_1: 1 and 2 tasks

---
+++ home_work_1/src/HomeWorkApp.java
@@ -0,0 +1,18 @@
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        /*
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+        */
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.print("Orange\n");
+        System.out.print("Banana\n");
+        System.out.print("Apple\n");
+    }
+
+}
