167f676bc185bfad7f299e922ce3ed56604eacca
m.nosova <xiatelly@gmail.com>
2024-02-13 19:26:35 +0200

    hw1: code review fixes

--- hw1/src/HomeWorkApp.java
+++ hw1/src/HomeWorkApp.java
@@ -4,7 +4,7 @@ public class HomeWorkApp {
     public static void main(String[] args) {
         HomeWorkApp app = new HomeWorkApp();
 
-        final int testDigit = 15;
+        final int TEST_DIGIT = 15;
 
         System.out.println("##Task2##");
         app.printThreeWords();
@@ -19,21 +19,22 @@ public class HomeWorkApp {
         app.compareNumbers();
 
         System.out.println("##Task6##");
-        app.checkDigitSum();
+        boolean task6Result = app.checkDigitSum();
+        System.out.println(task6Result);
 
         System.out.println("##Task7##");
-        app.checkDigitSign(testDigit);
+        app.checkDigitSign(TEST_DIGIT);
 
         System.out.println("##Task8##");
-        boolean task8Result = app.isDigitPositive(testDigit);
-        System.out.printf("%b%n", task8Result);
+        boolean task8Result = app.isDigitPositive(TEST_DIGIT);
+        System.out.println(task8Result);
 
         System.out.println("##Task9##");
         app.printString("Current String", 3);
 
         System.out.println("##Task10##");
         boolean yearCheck = app.leapYearCheck(2032);
-        System.out.printf("%b%n", yearCheck);
+        System.out.println(yearCheck);
     }
 
     private void printThreeWords() {
@@ -43,65 +44,62 @@ public class HomeWorkApp {
     }
 
     private void checkSumSign() {
-        final int a = 15;
-        final int b = -34;
+        int a = 15;
+        int b = -34;
+        int c = a + b;
 
-        final int c = a + b;
         if (c >= 0) {
             System.out.println("The Sum is positive");
-        } else {
-            System.out.println("The Sum is negative");
+            return;
         }
+
+        System.out.println("The Sum is negative");
     }
 
     private void printColor(){
-        final int digit = 25;
+        int digit = 25;
 
         if (digit <= 0) {
             System.out.print("Red\n");
-        } else if(digit >0 && digit <=100) {
-            System.out.print("Yellow\n");
-        } else if (digit >100) {
-            System.out.print("Green\n");
+            return;
         }
+
+        if(digit >0 && digit <=100) {
+            System.out.print("Yellow\n");
+            return;
+        }
+
+        System.out.print("Green\n");
     }
 
     private void compareNumbers(){
-        final int a = 34;
-        final int b = 44;
+        int a = 34;
+        int b = 44;
 
         if (a >= b){
             System.out.print("a >= b\n");
+            return;
         }
-        else {
-            System.out.print("a < b\n");
-        }
+
+        System.out.print("a < b\n");
     }
 
-    private void checkDigitSum(){
-        boolean sumAB = true;
+    private boolean checkDigitSum(){
         System.out.print("Input a: ");
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
         System.out.print("Input b: ");
         int b = scan.nextInt();
-
-        if (a + b >= 10 && a + b <= 20 ){
-            System.out.printf("%b%n", sumAB);
-        }
-        else {
-            sumAB = false;
-            System.out.printf("%b%n", sumAB);
-        }
+        return (a + b >= 10 && a + b <= 20);
     }
 
     private void checkDigitSign(int digit){
         if (digit >= 0){
             System.out.print("Its a positive number\n");
+            return;
         }
-        else{
-            System.out.print("Its a negative number\n");
-        }
+
+        System.out.print("Its a negative number\n");
     }
 
     private boolean isDigitPositive(int digit){
@@ -109,20 +107,13 @@ public class HomeWorkApp {
     }
 
     private void printString(String str, int count){
-
-        for (int i = 0; i< count; i++){
+        for (int i = 0; i< count; i++)
             System.out.println(str);
-        }
     }
 
     private boolean leapYearCheck(int year){
-        final boolean yearDevidingBy4 = (year%4 == 0);
-        final boolean yearDevidingBy100 = (year%100 == 0);
-        final boolean yearDevidingBy400 = (year%400 == 0);
-
-        if (yearDevidingBy4 && (!yearDevidingBy100 || yearDevidingBy400)){
-            return true;
-        }
-        return false;
+        if (year%4 != 0)
+            return false;
+        return (year%100 != 0 || year%400 == 0);
     }
 }
