a85ad4a685efae94da308e96cb46c54ac5650e03
m.nosova <xiatelly@gmail.com>
2024-02-28 00:23:14 +0200

    hw8: tasks 0-5 done

---
+++ hw8/src/FileData.java
@@ -0,0 +1,24 @@
+public class FileData {
+    private String name;
+
+    private int size;
+
+    private String path;
+
+    public FileData(String name, int size, String path){
+        this.name = name;
+        this.size = size;
+        this.path = path;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getPath() {
+        return path;
+    }
+}
---
+++ hw8/src/FileNavigator.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class FileNavigator {
+
+    private HashMap<String, List<FileData>> data = new HashMap<>();
+
+    public void add(String name, int size, String path){
+        add(new FileData(name, size, path));
+    }
+
+    public void add(FileData file){
+        List<FileData> record = data.get(file.getPath());
+        if (record == null){
+            List<FileData> newRecord = new ArrayList<>();
+            newRecord.add(file);
+            data.put(file.getPath(), newRecord);
+        }
+        else{
+            record.add(file);
+        }
+    }
+
+    public final List<FileData> find(String pathK){
+        return data.get(pathK);
+    }
+
+    public List<FileData> filterBySize(int size){
+        List<FileData> result = new ArrayList<>();
+
+        data.forEach(
+            (path, record) -> {
+                for(FileData fileData : record)
+                    if(fileData.getSize() <= size)
+                        result.add(fileData);
+            }
+        );
+        return result;
+    }
+
+    public void remove(String pathR){
+        data.remove(pathR);
+    }
+
+    public final HashMap<String, List<FileData>> getData() {
+        return data;
+    }
+}
