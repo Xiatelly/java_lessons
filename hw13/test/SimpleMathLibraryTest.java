a66251249de23333633e789d2b854ff3e3d9a0c5
m.nosova <xiatelly@gmail.com>
2024-05-01 17:44:57 +0300

    hw13: Done

--- hw13/test/SimpleMathLibraryTest.java
+++ hw13/test/SimpleMathLibraryTest.java
@@ -1,28 +1,28 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SimpleMathLibraryTest {
+public class SimpleMathLibraryTest {
     @Test
-    void checkAddTwoAndTwo(){
+    public void checkAddTwoAndTwo(){
         SimpleMathLibrary library = new SimpleMathLibrary();
         assertEquals(4.0, library.add(2.0, 2.0));
     }
 
     @Test
-    void checkAddFiveAndTwo(){
+    public void checkAddFiveAndTwo(){
         SimpleMathLibrary library = new SimpleMathLibrary();
         assertEquals(7.0, library.add(5.0, 2.0));
     }
 
     @Test
-    void checkMinusTenAndOne(){
+    public void checkMinusTenAndOne(){
         SimpleMathLibrary library = new SimpleMathLibrary();
         assertEquals(9.0, library.minus(10.0, 1.0));
     }
 
     @Test
-    void checkMinusTenAndTwelve(){
+    public void checkMinusTenAndTwelve(){
         SimpleMathLibrary library = new SimpleMathLibrary();
         assertEquals(-2.0, library.minus(10.0, 12.0));
     }
--- hw13/test/TestResultParser.java
+++ hw13/test/TestResultParser.java
@@ -1,10 +1,51 @@
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
+import java.nio.file.Path;
+import java.util.Date;
 
 public class TestResultParser {
-//    public String parse(String path){
-//        byte[] encodedBytes = Files.readAllBytes(Paths.get(path));
-//        return new String(encodedBytes, StandardCharsets.UTF_8);
-//    }
+    public TestResult parse(String filePath){
+        return parse(new File(filePath));
+    }
+
+    public TestResult parse(Path filePath){
+        return parse(new File(filePath.toString()));
+    }
+
+    public TestResult parse(File file){
+        TestResult result = new TestResult();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                parseConfigurationLine(result, line);
+            }
+            result.timeTests = new Date(file.lastModified());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private void parseConfigurationLine(TestResult result, String line){
+        int colonIndex = line.indexOf(":");
+
+        if (colonIndex <= 0  || line.length() < colonIndex + 1) {
+            return;
+        }
+
+        String key = line.substring(0, colonIndex).trim();
+        String value = line.substring(colonIndex + 1).trim();
+
+        switch (key) {
+            case TestRunnerConfig.LOG_TESTS_RUN_KEY:
+                result.totalTests = Integer.parseInt(value);
+                break;
+            case TestRunnerConfig.LOG_TESTS_FAILURES_KEY:
+                result.failedTests = Integer.parseInt(value);
+                result.succeededTests = result.totalTests - result.failedTests;
+                break;
+        }
+    }
 }
