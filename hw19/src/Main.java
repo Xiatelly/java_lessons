d06f167eef447b43400580398ea8e2516d55b5d9
m.nosova <xiatelly@gmail.com>
2024-05-20 23:55:02 +0300

    hw19: extra task with concurrency package done

--- hw19/src/Main.java
+++ hw19/src/Main.java
@@ -1,14 +1,28 @@
+import java.util.concurrent.ExecutionException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Main main = new Main();
-        main.valueCalculatorTest();
+        main.testValueCalculator();
+        System.out.println();
+        main.testConcurrentValueCalculator();
     }
 
-    private void valueCalculatorTest(){
+    private void testValueCalculator(){
         ValueCalculator calculator = new ValueCalculator();
         try {
             calculator.calculate();
         } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void testConcurrentValueCalculator() {
+        ConcurrentValueCalculator calculator = new ConcurrentValueCalculator();
+        try {
+            calculator.calculate();
+        }
+        catch (ExecutionException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
